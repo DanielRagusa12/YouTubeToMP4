360deabe8ff52ba9afdc268514154bc5a2e3d276
DanielRagusa12 <daniel.ragusa129@gmail.com>
2022-10-12 20:17:51 -0400

    V2

--- YouTubeToMP4.py
+++ YouTubeToMP4.py
@@ -1,95 +1,134 @@
-
+import pytube
 from pytube import YouTube
 from sys import argv
 import os
+from pytube.cli import on_progress
+import re
+import sys
 
 
+def download_complete(stream, file_path):
+    print("\n")
+    print("Downloaded to: " + file_path)
+    print("\n")
 
 
-
-def get_vid(tube):
-
-    print("Title: ",tube.title)
-    video = tube.streams.get_highest_resolution()
-
-
-    print("------------------OUTPUT-----------------")
-
-
+def get_dir():
 
     current_dir = os.getcwd()
-    print("1: Download to Project Directory: "+current_dir)
+
+    print("1: Download to Project Directory: " + current_dir)
     print("2: Custom Directory: ")
 
     option = str(input())
-    dir = ''
+    dir = ""
 
-    if(option == '2'):
-        print("Enter Dir: ",end='')
+    if option == "2":
+        print("Enter Dir: ", end="")
         dir = str(input())
 
     else:
         dir = current_dir
 
-
-
     while os.path.exists(dir) == False:
         print("Directory Invalid. Please try again.")
-        print("Enter Dir: ",end='')
+        print("Enter Dir: ", end="")
         dir = str(input())
 
- 
-    video.download(str(dir), skip_existing = True)
+    return dir
 
 
-    print("------------------SUCCESS-----------------")
-    print("Downloaded to: "+dir)
-        
+def get_audio(tube):
+
+    audio = tube.streams.get_audio_only()
+
+    parent_dir = str(get_dir())
+
+    path = os.path.join(parent_dir, "AUDIO")
+
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+
+    fileName = re.sub("\W+", "", tube.title)
+
+    audio.download(output_path=path, filename=fileName + ".mp3", skip_existing=True)
 
 
-print("-----------------YOUTUBE-----------------")
-print("------------------VIDEO------------------")
-print("----------------DOWNLOADER---------------")
-print("-----------------------------------------")
-print("WARNING: You will be promted to sign into your YouTube acccount on 1st launch using OAuth")
+def get_vid(tube):
+
+    video = tube.streams.get_highest_resolution()
+
+    parent_dir = str(get_dir())
+
+    path = os.path.join(parent_dir, "VIDEO")
+
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+
+    fileName = re.sub("\W+", "", tube.title)
+
+    video.download(output_path=path, filename=fileName + ".mp4", skip_existing=True)
 
 
+# ----------START------------
+
+
+print(
+    "-------------------------------------------------YOUTUBE---------------------------------------------------"
+)
+print(
+    "--------------------------------------------------VIDEO----------------------------------------------------"
+)
+print(
+    "------------------------------------------------DOWNLOADER-------------------------------------------------"
+)
+print(
+    "-----------------------------------------------------------------------------------------------------------"
+)
+print(
+    "WARNING: You will be promted to sign into your YouTube acccount on 1st launch using OAuth"
+)
+
+
+left_alignment = "Left Text"
+center_alignment = "Centered Text"
+right_alignment = "Right Text"
+  
+# printing out aligned text
+print(f"{left_alignment : <20}{center_alignment : ^15}{right_alignment : >20}")
 
 
 try:
-    print("video URL: ",end = '')
+    print("video URL: ", end="")
+
     link = str(input())
-    tube = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-    tube.check_availability()
 
-    
-    
-    get_vid(tube)
+    tube = YouTube(
+        link, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress
+    )
 
+    tube.register_on_complete_callback(download_complete)
+
+    print("Title: ", tube.title)
+
+    print(
+        "-------------------------------------------------OUTPUT---------------------------------------------------_"
+    )
+    print(
+        "-------------------------------------------------OPTIONS---------------------------------------------------"
+    )
+
+    print("1. Video .MP4\n2. Audio .MP3")
+
+    out_choice = str(input())
+
+    if out_choice == "1":
+        get_vid(tube)
+
+    elif out_choice == "2":
+        get_audio(tube)
 
 
 except:
     print("ERROR")
     print("APP CLOSING...")
-    
-
-
-
-
-
-
-
-
-
-    
-
-        
-
-
-
-
-
-
-
-
-
